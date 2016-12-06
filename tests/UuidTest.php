fe981a0a7d44542e09b13932de5c9658a5855ecd
oscarotero <oom@oscarotero.com>
2016-12-06 18:22:22 +0100

    updated tests

--- tests/UuidTest.php
+++ tests/UuidTest.php
@@ -4,20 +4,17 @@ namespace Middlewares\Tests;
 
 use Middlewares\Uuid;
 use Middlewares\Utils\Dispatcher;
-use Middlewares\Utils\Factory;
 
 class UuidTest extends \PHPUnit_Framework_TestCase
 {
     public function testUuid()
     {
-        $request = Factory::createServerRequest();
-
-        $response = (new Dispatcher([
+        $response = Dispatcher::run([
             new Uuid(),
             function ($request) {
                 echo $request->getHeaderLine('X-Uuid');
             },
-        ]))->dispatch($request);
+        ]);
 
         $this->assertInstanceOf('Psr\\Http\\Message\\ResponseInterface', $response);
         $this->assertRegExp(
